2dfa7fbe05fef715ca9f66c6c42a7b099e12f609
KongKunta <kongkunta@yahoo.com>
2023-10-30 23:14:02 +0100

    added Alerts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './Navbar'
 import Layout from './Layout'
 import Accordion from './Accordion'
+import Alerts from './Alerts'
 
 function App(){
   return (
     <div>
       <Navbar />
       <Layout><Accordion /></Layout>
+      <Layout><Alerts /></Layout>
     </div>
   )
 }
